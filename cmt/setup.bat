7181a0eefe3e0c0b826948963196adb4e30f34c2
riccardo <>
2004-07-08 13:01:46 +0000

    Small changes to the requirements file for the new exteranl libraries in IExternal

--- cmt/setup.bat
+++ cmt/setup.bat
@@ -1,7 +1,8 @@
 @echo off
 if .%1==. (set tag=VC8 ) else set tag=%1
+set CMTPATH=e:\Ric\glast\GlastRelease43;
 set tempfile=%HOME%\tmpsetup.bat
-e:\Ric\glast\CMT\v1r14p20030620\VisualC\cmt.exe -quiet -bat -pack=HepRepXml -version=v0r3 setup -tag=%tag% >"%tempfile%"
+e:\Ric\glast\CMT\v1r14p20031120\VisualC\cmt.exe -quiet -bat -pack=HepRepXml -version=v0r6 setup -tag=%tag% >"%tempfile%"
 if exist "%tempfile%" call "%tempfile%"
 if exist "%tempfile%" del "%tempfile%"
 set PATH=%LD_LIBRARY_PATH%;%PATH%
