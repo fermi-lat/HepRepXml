185a8358b79417e5d203515d4d3efe75f2fde696
riccardo <>
2004-07-20 15:14:57 +0000

    Prepare for v0r6 tag

--- cmt/setup.bat
+++ cmt/setup.bat
@@ -1,8 +1,8 @@
 @echo off
 if .%1==. (set tag=VC8 ) else set tag=%1
-set CMTPATH=e:\Ric\glast\GlastRelease43;
+set CMTPATH=e:\Ric\glast\GlastRelease43;e:\Ric\glast\temp;e:\Ric\glast;
 set tempfile=%HOME%\tmpsetup.bat
-e:\Ric\glast\CMT\v1r14p20031120\VisualC\cmt.exe -quiet -bat -pack=HepRepXml -version=v0r6 setup -tag=%tag% >"%tempfile%"
+e:\Ric\glast/CMT/v1r14p20031120/VisualC/cmt.exe -quiet -bat -pack=HepRepXml -version=v0r6 setup -tag=%tag% >"%tempfile%"
 if exist "%tempfile%" call "%tempfile%"
 if exist "%tempfile%" del "%tempfile%"
 set PATH=%LD_LIBRARY_PATH%;%PATH%
