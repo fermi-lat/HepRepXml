57425010f4d175647735e9ea5873dabe626cb0e7
riccardo <>
2004-09-22 16:46:19 +0000

    Migration to new HepRep2 real support; instances are now saved with full name qualified.

--- cmt/setup.bat
+++ cmt/setup.bat
@@ -1,8 +1,8 @@
 @echo off
 if .%1==. (set tag=VC8 ) else set tag=%1
-set CMTPATH=e:\Ric\glast\GlastRelease43;e:\Ric\glast\temp;e:\Ric\glast;
+set CMTPATH=e:\Ric\glast\GlastRelease44;e:\Ric\glast\temp;e:\Ric\glast;
 set tempfile=%HOME%\tmpsetup.bat
-e:\Ric\glast/CMT/v1r14p20031120/VisualC/cmt.exe -quiet -bat -pack=HepRepXml -version=v0r6 setup -tag=%tag% >"%tempfile%"
+e:\Ric\glast/CMT/v1r16p20040701/VisualC/cmt.exe -quiet -bat -pack=HepRepXml -version=v0r7 setup -tag=%tag% >"%tempfile%"
 if exist "%tempfile%" call "%tempfile%"
 if exist "%tempfile%" del "%tempfile%"
 set PATH=%LD_LIBRARY_PATH%;%PATH%
