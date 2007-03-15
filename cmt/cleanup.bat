1360a20c525d4f58cabe34709855b8f42745c4a8
lsrea <>
2007-03-15 20:27:00 +0000

    prepare for tag v0r7p1 fix test program

--- cmt/cleanup.bat
+++ cmt/cleanup.bat
@@ -1,8 +1,8 @@
 @echo off
-if NOT DEFINED CMTROOT set CMTROOT=e:\Ric\glast\CMT\v1r14p20031120 & set PATH=%CMTROOT%\%CMTBIN%;%PATH% & set CMTBIN=VisualC & if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
+if NOT DEFINED CMTROOT set CMTROOT=C:\Programs\CMT\v1r18p20060301& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
 
 set cmttempfile="%TEMP%\tmpsetup.bat"
-%CMTROOT%\%CMTBIN%\cmt.exe -quiet cleanup -bat  -pack=HepRepXml -version=v0r6 -path=%~d0%~p0..\..\..   %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
+%CMTROOT%\%CMTBIN%\cmt.exe cleanup -bat  -pack=HepRepXml -version=v0r7 -path="%~d0%~p0..\..\.."   %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
 if exist %cmttempfile% call %cmttempfile%
 if exist %cmttempfile% del %cmttempfile%
 set cmttempfile=
--- cmt/setup.bat
+++ cmt/setup.bat
@@ -1,8 +1,10 @@
+rem Setting HepRepXml v0r7 in %~d0%~p0
 @echo off
-if .%1==. (set tag=VC8 ) else set tag=%1
-set CMTPATH=e:\Ric\glast\GlastRelease44;e:\Ric\glast\temp;e:\Ric\glast;
-set tempfile=%HOME%\tmpsetup.bat
-e:\Ric\glast/CMT/v1r16p20040701/VisualC/cmt.exe -quiet -bat -pack=HepRepXml -version=v0r7 setup -tag=%tag% >"%tempfile%"
-if exist "%tempfile%" call "%tempfile%"
-if exist "%tempfile%" del "%tempfile%"
-set PATH=%LD_LIBRARY_PATH%;%PATH%
+if NOT DEFINED CMTROOT set CMTROOT=C:\Programs\CMT\v1r18p20060301& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
+
+set cmttempfile="%TEMP%\tmpsetup.bat"
+%CMTROOT%\%CMTBIN%\cmt.exe setup -bat  -pack=HepRepXml -version=v0r7 -path="%~d0%~p0..\..\.."   -no_cleanup %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
+if exist %cmttempfile% call %cmttempfile%
+if exist %cmttempfile% del %cmttempfile%
+set cmttempfile=
+
