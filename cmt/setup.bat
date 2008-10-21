d3f737ff51713992087b0ce91a0733e02019d2f0
lsrea <>
2008-10-21 18:55:55 +0000

    get test job running again... the .bat files shouldn't be in cvs!

--- cmt/setup.bat
+++ cmt/setup.bat
@@ -1,9 +1,9 @@
-rem Setting HepRepXml v0r7 in %~d0%~p0
+rem Setting HepRepXml v0r8p2 in %~d0%~p0
 @echo off
-if NOT DEFINED CMTROOT set CMTROOT=C:\Programs\CMT\v1r18p20060301& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
+if NOT DEFINED CMTROOT set CMTROOT=c:\GlastPrograms\CMT\v1r18p20061003& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
 
 set cmttempfile="%TEMP%\tmpsetup.bat"
-%CMTROOT%\%CMTBIN%\cmt.exe setup -bat  -pack=HepRepXml -version=v0r7 -path="%~d0%~p0..\..\.."   -no_cleanup %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
+%CMTROOT%\%CMTBIN%\cmt.exe setup -bat  -pack=HepRepXml -version=v0r8p2 -path="%~d0%~p0..\..\.."   -no_cleanup %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
 if exist %cmttempfile% call %cmttempfile%
 if exist %cmttempfile% del %cmttempfile%
 set cmttempfile=
